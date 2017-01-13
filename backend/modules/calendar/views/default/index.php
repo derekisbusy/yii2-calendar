45224fdcfd63fee34ce0c83023975c58e626b6b4
Derek Gifford <derekisbusy@gmail.com>
2017-01-13 08:37:34 -0500

    Update index.php.

--- backend/modules/calendar/views/default/index.php
+++ backend/modules/calendar/views/default/index.php
@@ -5,7 +5,7 @@
 $this->registerJs('var appointmentUrl = "' . yii\helpers\Url::to('/backend/appointment') . '";', yii\web\View::POS_HEAD);
 derekisbusy\calendar\backend\assets\ScheduleAsset::register($this);
 
-echo \derekisbusy\calendar\widgets\FullCalendarWidget::widget(['id'=>'calendar',
+echo \derekisbusy\calendar\widgets\fullcalendar\FullCalendarWidget::widget(['id'=>'calendar',
     'pluginOptions'=>[
         'events'=>\yii\helpers\Url::toRoute('calendar-events'),
         'eventClick'=>new yii\web\JsExpression('calendarEventClick')]]);
